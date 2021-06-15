625fcc852462152c39fca1aff9e1fb1235adaf0d
Mohammad Ilham Nurdhi Hafsyah <ilhamhafsyah15@gmail.com>
2021-06-15 11:40:15 +0700

    add procfile

--- src/config/postgre.js
+++ src/config/postgre.js
@@ -1,10 +1,10 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "perusahaan",
-  password: "1234",
+  user: "jocpfilzyhxotw",
+  host: "ec2-54-90-211-192.compute-1.amazonaws.com",
+  database: "d6g82gskp94q87",
+  password: "7b84573efb43a8761a804983966d7a1c416a2283d121a7ee84ea201abb513a10",
   port: 5432,
 });
 
